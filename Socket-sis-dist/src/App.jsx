a1aa691f327bae27e5a9b8e2eb0bd9794b4bce08
Fabian Duran <ingfabianavellaneda@gmail.com>
2023-03-21 17:26:46 -0500

    Commit inicial configuracion sockets

---
+++ Socket-sis-dist/src/App.jsx
@@ -0,0 +1,8 @@
+import socketIo from "socket.io-client";
+const socket = socketIo.connect("http://localhost:3000");
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
---
+++ Socket-sis-dist/src/components/FormularioPersona.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Card from "@mui/material/Card";
+
+export default function FormularioPersona() {
+  return <div>FormularioPersona</div>;
+}
